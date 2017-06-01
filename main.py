8ef7abb1c22070d039f7de3f04994e41166e7e62
Jason Hwa <jhwa@sumologic.com>
2017-06-01 12:01:32 -0700

    reversion
    Troubleshooting

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template("home.html")
-
-@app.route('/about/')
-def about():
-    return render_template("about.html")
+def hello_world():
+  return 'Hello, World!'
 
 if __name__ == '__main__':
   app.run()
